6653a9793fffc51aed1c6dbec5547d6d8630e31a
Douglas Batista <dobatista7@gmail.com>
2024-05-18 11:29:45 -0300

    criação do projeto e inclusão do arquivo .gitignore

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace desafioBarOO
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
