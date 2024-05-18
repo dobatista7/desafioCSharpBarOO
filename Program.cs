40b234f35c63c7a3d1a55e61161fb960d0e3e081
Douglas Batista <dobatista7@gmail.com>
2024-05-18 12:02:51 -0300

    elaboração dos dados de saída do programa - Relatório

--- Program.cs
+++ Program.cs
@@ -23,6 +23,22 @@ namespace desafioBarOO
             double ingresso = bill.ticket();
             double conta = bill.total();
 
+            Console.WriteLine();
+            Console.WriteLine("RELATÓRIO: ");
+            Console.WriteLine("CONSUMO: R$  "+ consumo.ToString("F2",CultureInfo.InvariantCulture));
+            if (consumo > 30.00)
+            {
+                Console.WriteLine("Insento de Couvert");
+            }else
+            {
+                Console.WriteLine("Couvert: R$  " + couvert.ToString("F2", CultureInfo.InvariantCulture));
+            }
+            Console.WriteLine("Ingresso: R$ " + ingresso.ToString("F2", CultureInfo.InvariantCulture));
+
+            Console.WriteLine();
+
+            Console.WriteLine("Valor a pagar = R$ " + conta.ToString("F2", CultureInfo.InvariantCulture));
+
 
         }
     }
