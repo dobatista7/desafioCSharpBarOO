12d5fbb63910d2fdbfcdc40e0d6b564a4012e2b0
Douglas Batista <dobatista7@gmail.com>
2024-05-18 11:47:18 -0300

    criação das variáveis de entrada no programa principal

--- Program.cs
+++ Program.cs
@@ -1,10 +1,23 @@
-﻿namespace desafioBarOO
+﻿using System;
+using System.Globalization;
+
+namespace desafioBarOO
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Bill bill = new Bill();
+
+            Console.Write("Sexo: ");
+            bill.gender = char.Parse(Console.ReadLine());
+            Console.Write("Quantidade de cervejas: ");
+            bill.beer = int.Parse(Console.ReadLine());
+            Console.Write("Quantidade de refrigerantes: ");
+            bill.softDrink = int.Parse(Console.ReadLine());
+            Console.Write("Quantidade de espetinhos: ");
+            bill.barbecue = int.Parse(Console.ReadLine());
+
         }
     }
 }
